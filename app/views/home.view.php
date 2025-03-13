a1212eb40d1f0c5a6384e41606bd78678d6e470a
Clodoaldo Dantas <clodoaldodantas8@gmail.com>
2025-03-13 16:57:44 -0300

    conditionally render home page content based on authentication status

--- app/views/home.view.php
+++ app/views/home.view.php
@@ -11,21 +11,34 @@
       ferramentas de organização poderosas e armazenamento seguro.
     </p>
 
-    <div class="flex flex-wrap items-center justify-center gap-4">
-      <a
-        href="/sign-up"
-        class="px-4 py-2 rounded text-white text-lg bg-blue-600 hover:bg-blue-500 flex items-center gap-2">
-        <i class="ph-bold ph-user-plus"></i>
-        Inicie agora
-      </a>
+    <?php $isAuthenticated = Core\Session::has("user"); ?>
 
-      <a
-        href="/sign-in"
-        class="px-4 py-2 rounded text-lg text-zinc-600 border border-input bg-transparent hover:bg-zinc-600 hover:text-white flex items-center gap-2">
-        <i class="ph-bold ph-sign-in"></i>
-        Fazer login
-      </a>
-    </div>
+    <?php if ($isAuthenticated) : ?>
+      <div class="flex flex-wrap items-center justify-center gap-4">
+        <a
+          href="/contacts"
+          class="px-4 py-2 rounded text-lg text-zinc-600 border border-input bg-transparent hover:bg-zinc-600 hover:text-white flex items-center gap-2">
+          <i class="ph-bold text-xl ph-address-book"></i>
+          Minha Agenda
+        </a>
+      </div>
+    <?php else : ?>
+      <div class="flex flex-wrap items-center justify-center gap-4">
+        <a
+          href="/sign-up"
+          class="px-4 py-2 rounded text-white text-lg bg-blue-600 hover:bg-blue-500 flex items-center gap-2">
+          <i class="ph-bold ph-user-plus"></i>
+          Inicie agora
+        </a>
+
+        <a
+          href="/sign-in"
+          class="px-4 py-2 rounded text-lg text-zinc-600 border border-input bg-transparent hover:bg-zinc-600 hover:text-white flex items-center gap-2">
+          <i class="ph-bold ph-sign-in"></i>
+          Fazer login
+        </a>
+      </div>
+    <?php endif; ?>
   </div>
 </div>
 
